b13f5802ed7d3fab529dbe331a06628b3a3a9e60
Shota <eg.shota@yandex.ru>
2023-07-21 12:23:14 +0300

    MovementUpdate
    +PlayerAnimation
    * Jump animation code need some rework
    +Ground checker

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private PlayermMovement _playerMovement;
+
+    private Animator _animator;
+    private Animation _animation;
+    private string _previousAnimationName;
+
+    private void OnEnable()
+    {
+        _playerMovement.OnMovementMade += MakeAnimation;
+    }
+
+    private void OnDisable()
+    {
+        _playerMovement.OnMovementMade -= MakeAnimation;
+    }
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _animation = GetComponent<Animation>();
+    }
+
+    private void MakeAnimation(string animationName)
+    {
+        Debug.Log(animationName);
+        _animator.Play(animationName);
+    }
+}
--- Assets/Scripts/Player/PlayermMovement.cs
+++ Assets/Scripts/Player/PlayermMovement.cs
@@ -5,32 +5,46 @@ using UnityEngine.Events;
 
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayermMovement : MonoBehaviour
 {
     [SerializeField] float _speed;
     [SerializeField] float _jumpForce;
+    [SerializeField] LayerMask _layerMask;
 
+    private CapsuleCollider2D _collider;
     private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody2d;
+
+    public event UnityAction<string> OnMovementMade;
 
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _rigidbody2d = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<CapsuleCollider2D>();
     }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.A))
             Move(Vector2.left);
+
         if (Input.GetKey(KeyCode.D))
             Move(Vector2.right);
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if ((Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) && IsGrounded())
+            OnMovementMade?.Invoke("Idle");
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
             Jump();
     }
 
     private void Move(Vector2 direction)
     {
+        if (IsGrounded())
+            OnMovementMade?.Invoke("Run");
+
         _spriteRenderer.flipX = direction.x < 0;
         transform.Translate(direction * _speed * Time.deltaTime);
     }
@@ -38,5 +52,20 @@ public class PlayermMovement : MonoBehaviour
     private void Jump()
     {
         _rigidbody2d.velocity = Vector2.up * _jumpForce;
+        OnMovementMade?.Invoke("Jump"); //перебивается бегом + добавить бы для падения эту анимацю возможно...
+    }
+
+    private bool IsGrounded()
+    {
+        float angle = 0f;
+        float rayLength = 0.05f;
+        float boxCastSizeY = 0.1f;
+        float boxCastSizeX = _collider.size.x;
+
+        Vector2 feetCenter = new Vector2(_collider.bounds.min.x + _collider.size.x / 2, _collider.bounds.min.y);
+        Vector2 feetBox = new Vector2(boxCastSizeX, boxCastSizeY);
+        RaycastHit2D groundRay = Physics2D.BoxCast(feetCenter, feetBox, angle, Vector2.down, rayLength, _layerMask);
+
+        return groundRay.collider != null;
     }
 }
